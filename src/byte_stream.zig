8605da41ed0a56b60ff449056859c79e4e604af6
unnask <8060462+unnask@users.noreply.github.com>
2023-09-06 23:11:18 -0600

    ex

--- src/byte_stream.zig
+++ src/byte_stream.zig
@@ -1,10 +1,47 @@
 const std = @import("std");
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+//quick example of an optional type parameter
+
+pub fn fixedByteStream() ByteStream {
+    return ByteStream(null);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+pub const FixedByteStream = ByteStream(null);
+
+pub fn managedByteStream(comptime allocator: Allocator) ByteStream {
+    return ByteStream(allocator);
+}
+
+fn ByteStream(comptime allocator: ?Allocator) type {
+    return struct {
+        const Self = @This();
+
+        capacity: usize,
+        bytes: []u8,
+
+        pub const init = if (allocator) |_| initCapacity else initFixed;
+
+        fn initCapacity(capacity: usize) Allocator.Error!Self {
+            var alloc = if (allocator) |a| a else @compileError("initCapacity requires allocator. Use managedByteStream.");
+            var bytes = try alloc.alloc(u8, capacity);
+            bytes.len = 0;
+
+            return .{
+                .capacity = capacity,
+                .bytes = bytes,
+            };
+        }
+
+        fn initFixed(buffer: []u8) Self {
+            const capacity = buffer.len;
+            buffer.len = 0;
+
+            return .{
+                .capacity = capacity,
+                .bytes = buffer,
+            };
+        }
+    };
 }
